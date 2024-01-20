1db473160a7b10a7df6fe8a53ef6d904f511f03f
GagoChad <gagika57@gmail.com>
2024-01-20 19:50:57 +0500

    feat: add event listener to results list

--- src/components/SearchResult.jsx
+++ src/components/SearchResult.jsx
@@ -1,8 +1,9 @@
-const SearchResult = ({ result }) => {
-	
+const SearchResult = ({ result, isActive }) => {
+	const activeClass = isActive ? 'bg-blue-500' : ''
+
 	return (
 		<div
-			className='text-xl pl-4 py-4 text-left text-white hover:text-black hover:bg-slate-300  hover:rounded-md cursor-pointer'
+			className={`text-xl pl-4 py-4 text-left text-white hover:text-black hover:bg-slate-300  hover:rounded-md cursor-pointer ${activeClass}`}
 		>
 			{result.name}
 		</div>
--- src/components/SearchResultList.jsx
+++ src/components/SearchResultList.jsx
@@ -1,15 +1,32 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 import NotFound from './NotFound'
 import SearchResult from './SearchResult'
 
 const SearchResultList = ({ results }) => {
 	const [isNotFound, setIsNotFound] = useState(true)
+	const [activeIndex, setActiveIndex] = useState(0)
+
+	useEffect(() => {
+		const handleKeyDown = (event) => {
+			if (event.key === 'ArrowDown') {
+				setActiveIndex((prevIndex) => Math.min(prevIndex + 1, results.length - 1))
+			} else if (event.key === 'ArrowUp') {
+				setActiveIndex((prevIndex) => Math.max(prevIndex - 1, 0))
+			}
+		}
+
+		window.addEventListener('keydown', handleKeyDown)
+
+		return () => {
+			window.removeEventListener('keydown', handleKeyDown)
+		}
+	}, [results.length])
 
 	return (
 		<div className='w-100% bg-gray-700 mx-0 flex flex-col shadow-sm rounded-b-md max-h'>
 			{results.length > 0 || results === '' ? (
-				results.map((result, id) => <SearchResult key={id} result={result} />)
+				results.map((result, id) => <SearchResult key={id} result={result} isActive={id === activeIndex} />)
 			) : isNotFound ? (
 				<NotFound />
 			) : null}
